60880bca3b98ad29109677bd2db0a74d51a5ec02
santiagxf <fasantia@microsoft.com>
2022-01-07 21:56:52 -0500

    fix: metrics not logged

--- src/hatedetection/model/__main__.py
+++ src/hatedetection/model/__main__.py
@@ -1,9 +1,11 @@
 """
-This module runs the training job for the hate detection model
+This module runs the evaluation job for the hate detection model
 """
+import time
 from common.jobs.runner import TaskRunner
 from hatedetection.model.evaluation import resolve_and_compare
 
 if __name__ == "__main__":
     tr = TaskRunner()
     tr.run_and_log(resolve_and_compare)
+    time.sleep(5)
