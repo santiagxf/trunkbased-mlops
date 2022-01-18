7bc6f60fc20ff8a50650d00d059f5e9d9bb24712
santiagxf <fasantia@microsoft.com>
2022-01-18 10:37:18 -0300

    fix: coding style

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -3,6 +3,9 @@ PyTest configuration script
 """
 
 def pytest_addoption(parser):
+    """
+    Register argparse-style options.
+    """
     parser.addoption(
         "--ws-config-file",
         dest="ws_config_file",
@@ -12,5 +15,9 @@ def pytest_addoption(parser):
     )
 
 def pytest_generate_tests(metafunc):
+    """
+    Defines the custom parametrization schema for each test. It is called each
+    time a test is collected.
+    """
     if "ws_config_file" in metafunc.fixturenames:
         metafunc.parametrize("ws_config_file", metafunc.config.getoption("ws_config_file"))
