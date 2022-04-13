6c1bd1841e876577a5bde597582f9d2f444fb1dd
Facundo Santiago <santiagof@outlook.com>
2022-04-13 03:21:10 -0400

    fix: model inference

--- src/hatedetection/model/hate_detection_classifier.py
+++ src/hatedetection/model/hate_detection_classifier.py
@@ -100,14 +100,15 @@ class HateDetectionClassifier(PythonModel):
         inputs = self.tokenizer(list(data), padding=True, return_tensors='pt')
         predictions = self.model(**inputs)
         probs = torch.nn.Softmax(dim=1)(predictions.logits)
+        probs = probs.detach().numpy()
         
         logging.info("[INFO] Building results with hate probabilities")
         classes = probs.argmax(axis=1)
         confidence = probs.max(axis=1)
 
         data = data.reset_index()
-        data['hate'] = classes.detach().numpy()
-        data['confidence'] = confidence.detach().numpy()
+        data['hate'] = classes
+        data['confidence'] = confidence
 
         results = data[['index', 'hate', 'confidence']].groupby('index').agg({'hate': pd.Series.mode, 'confidence': 'mean' })
 
