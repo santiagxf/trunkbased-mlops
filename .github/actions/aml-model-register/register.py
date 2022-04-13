33ce66ffb9b739fa18084ab2b16e5fd023bf111e
Facundo Santiago <santiagof@outlook.com>
2022-04-13 00:56:14 -0400

    fix: registration

--- .github/actions/aml-model-register/register.py
+++ .github/actions/aml-model-register/register.py
@@ -1,4 +1,5 @@
 import logging
+import mlflow
 import azureml.core as aml
 
 from typing import List, Dict, Any
@@ -18,16 +19,6 @@ def get_dataset(workspace: aml.Workspace, name: str) -> aml.Dataset:
         dataset = None
     return dataset
 
-def get_run(workspace: aml.Workspace, run_id: str) -> aml.Run:
-    """
-    Gets a run from the workspace with a given ID.
-    """
-    try:
-        run = aml.Run.get(workspace=workspace, run_id=run_id)
-    except Exception:
-        run = None
-    return run
-
 def register(subscription_id: str, resource_group: str, workspace_name:str, name: str, model_path: str,
              description: str, run_id: str = None, datasets_id: List[str] = None, tags: Dict[str, Any] = None):
     """
@@ -42,17 +33,10 @@ def register(subscription_id: str, resource_group: str, workspace_name:str, name
         datasets = None
 
     if run_id:
-        logging.info(f"[INFO] Looking for run with ID {run_id}.")
+        logging.info(f"[INFO] Logging with MLflow {run_id}.")
 
-        model_run = get_run(workspace=ws, run_id=run_id)
-        if model_run:
-            model_run.register_model(model_name=name,
-                                     model_path=model_path,
-                                     description=description,
-                                     tags=tags, 
-                                     datasets=datasets)
-        else:
-            logging.error(f"[ERROR] Run with ID {run_id} couldn't been found. Model is not registered.")
+        mlflow.set_registry_uri(ws.get_mlflow_tracking_uri())
+        mlflow.register_model(f'runs:/{run_id}/{model_path}', name)
     else:
         aml.Model.register(workspace=ws, 
                            model_name=name,
